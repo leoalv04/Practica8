e8152147fb6e4ba0e73c0ba30fc217b1c09a2941
leoalv04 <159968841+leoalv04@users.noreply.github.com>
2025-10-16 13:34:11 -0600

    Create Ejercicio1.c

---
+++ Ejercicio1.c
@@ -0,0 +1,18 @@
+#include ‹stdio.h›
+int main()
+{
+int num, cont = 0;
+
+  printf("\a ---- Tabla de multiplicar ----\n");
+  printf("Ingrese un número: \n");
+  scanf("%d", &num) ;
+  printf("La tabla de multiplicar del %d es: \n", num);
+
+  while (++cont <= 10)
+{
+printf("%d x %d = %d\n", num, cont, num*cont);
+  cont = cont +1
+    }
+
+return 0;
+}
